d6a4c542dfaa77cee1f71f0d0e12400cd807e701
Justin Nguyen <justinnguyen@Justins-Air.attlocal.net>
2021-06-24 09:26:24 -0700

    Add track component

--- react-app/src/components/tracks/Track.js
+++ react-app/src/components/tracks/Track.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
+import ReactAudioPlayer from 'react-audio-player';
 
-function Track(props){
+function Track( {track} ){
     return (
         <div>
 
+            <h2> { track.artist } </h2>
+            <h3> { track.name } </h3>
+            <audio
+                src={track.url}
+                controls
+            />
         </div>
     )
 }
+
+export default Track;
--- react-app/src/components/tracks/TrackShow.js
+++ react-app/src/components/tracks/TrackShow.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Track from "./Track";
 
 function TrackShow() {
     const [track, setTrack] = useState({});
     // Notice we use useParams here instead of getting the params
     // From props. USE props instead
     const { trackId } = useParams();
-
+    
     useEffect(() => {
         if (!trackId) {
             return
@@ -24,17 +25,7 @@ function TrackShow() {
     }
 
     return (
-        <ul>
-            <li>
-                <strong>Track Id</strong> {track.id}
-            </li>
-            <li>
-                <strong>Track Name</strong> {track.name}
-            </li>
-            <li>
-                <strong>Track Url</strong> {track.url}
-            </li>
-        </ul>
+            <Track track={track}/>
     );
 }
 export default TrackShow;
